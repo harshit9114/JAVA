354a2de2d28a6413346ab12af36e32b074adba7d
HARSHIT <harshit6683@gmail.com>
2026-06-23 00:11:46 +0530

    Time: 4 ms (12.08%) | Memory: 43.7 MB (13.86%) - LeetSync

--- 682-baseball-game/baseball-game.java
+++ 682-baseball-game/baseball-game.java
@@ -1,27 +1,30 @@
 class Solution {
     public int calPoints(String[] operations) {
-   ArrayList<Integer> l = new ArrayList<>();
-   for(int i=0;i<operations.length;i++){
-   
-    if( operations[i].equals("C")){
-        l.remove(l.size()-1);
-    }
-    else if(operations[i].equals("+")){
-        int a= l.get(l.size()-1);
-        int b= l.get(l.size()-2);
-        l.add(a+b);
-    }
-    else if( operations[i].equals("D")){
-        l.add(l.get((l.size()-1))*2);
-    }
-    else{
-        l.add(Integer.parseInt(operations[i]));
-    }
-   }
-   int total=0;
-   for( int x:l){
-        total+=x;
-   }
-   return total;
+        Stack<Integer> st = new Stack<>();
+        for (String ops : operations) {
+            if (ops.equals("C")) {
+                st.pop();
+            }
+            else if (ops.equals("+")) {
+                int a = st.pop();
+                int b = st.pop();
+                st.push(b);
+                st.push(a);
+                st.push(a + b);
+            }
+            else if (ops.equals("D")) {
+                st.push(st.peek() * 2);
+            }
+            else {
+                st.push(Integer.parseInt(ops));
+            }
+        }
+
+        int total = 0;
+        while (!st.isEmpty()) {
+            total += st.pop();
+        }
+
+        return total;
     }
 }
