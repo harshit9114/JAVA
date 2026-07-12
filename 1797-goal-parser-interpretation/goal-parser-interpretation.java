24c5a8dbb0b18fb890013934dbd7b65f001767fe
HARSHIT <harshit6683@gmail.com>
2026-07-12 22:26:37 +0530

    Time: 3 ms (4.91%) | Memory: 43.4 MB (7.61%) - LeetSync

---
+++ 1797-goal-parser-interpretation/goal-parser-interpretation.java
@@ -0,0 +1,42 @@
+class Solution {
+    public String interpret(String s) {
+        StringBuilder str= new StringBuilder();
+        Stack <Character> st = new Stack<>();
+        for(int i=0;i<s.length();i++){
+            StringBuilder n = new StringBuilder ();
+            if( !st.isEmpty() && st.peek()=='(' && s.charAt(i)==')'){
+                n.append('o');
+                while(!st.isEmpty()){
+                    char c= st.pop();
+                    if(c!='('){
+                        n.append(c);
+                    }
+                }
+                n.reverse();
+                str.append(n);
+            }
+            else if(!st.isEmpty() &&  s.charAt(i)==')' && st.peek()!='('){
+                while (!st.isEmpty()){
+                    char c= st.pop();
+                    if(c!='('){
+                        n.append(c);
+                    }
+                }
+                 n.reverse();
+                str.append(n);
+            }
+            else{
+                st.push(s.charAt(i));
+            }
+        }
+        if(!st.isEmpty()){
+             StringBuilder rem = new StringBuilder();
+            while (!st.isEmpty()) {
+                rem.append(st.pop());
+            }
+            str.append(rem.reverse());
+        }
+        return str.toString();
+  
+    }
+}
