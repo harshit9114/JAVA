b53021ab9f773886efc42b2d1629c40cfdc4e6b4
HARSHIT <harshit6683@gmail.com>
2026-07-15 23:51:03 +0530

    Time: 72 ms (5.77%) | Memory: 47.1 MB (21.33%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -1,40 +1,39 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        int high=0;
         int low=0;
-        int result=Integer.MIN_VALUE;
-        char c[] = s.toCharArray();
-        HashMap<Character,Integer> map= new HashMap<>();
-        while(high<c.length){
-            map.put(c[high],map.getOrDefault(c[high],0)+1);
-            int len=high-low+1;
+        int high=0;
+        int ans=0;
+         HashMap<Character,Integer> map = new HashMap<>();
+        while(high<s.length()){
+            map.put(s.charAt(high),map.getOrDefault(s.charAt(high),0)+1);
+            int length= high-low+1;
             int f=find(map);
-            int diff=len-f;
+            int diff= length-f;
             while(diff>k){
-                int a=map.get(c[low]);
+                int a= map.get(s.charAt(low));
                 a--;
                 if(a==0){
-                    map.remove(c[low]);
+                    map.remove(s.charAt(low));
                 }
                 else{
-                    map.put(c[low],a);
+                    map.put(s.charAt(low),a);
                 }
                 low++;
-                f=find(map);
-                len=high-low+1;
-                diff=len-f;
+                 length= high-low+1;
+             f=find(map);
+             diff= length-f;
             }
-             len=high-low+1;
-        result=Math.max(result,len);
+             length= high-low+1;
+            ans=Math.max(ans,length);
             high++;
         }
-        return result;
+        return ans;
     }
-    public int find(HashMap<Character,Integer> map){
-        int max=0;
-        for(int val:map.values()){
-     max=Math.max(max,val);
+        public int find(HashMap<Character,Integer> map){
+            int max=0;
+            for(int x: map.values()){
+                max=Math.max(max,x);
+            }
+            return max;
         }
-     return max;
-    }
-}
+        }
