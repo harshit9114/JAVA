7cf607c4fc3a89ab0b9d3ad559014a3a757bf336
HARSHIT <harshit6683@gmail.com>
2026-07-22 09:04:28 +0530

    Time: 7 ms (38.32%) | Memory: 46.6 MB (74.28%) - LeetSync

--- 501-find-mode-in-binary-search-tree/find-mode-in-binary-search-tree.java
+++ 501-find-mode-in-binary-search-tree/find-mode-in-binary-search-tree.java
@@ -14,32 +14,30 @@
  * }
  */
 class Solution {
-public void order(TreeNode n,HashMap<Integer,Integer> map){
+    public void order(TreeNode n,HashMap<Integer,Integer> map){
         if(n==null){
             return;
         }
         map.put(n.val,map.getOrDefault(n.val,0)+1);
         order(n.left,map);
         order(n.right,map);
-       
     }
     public int[] findMode(TreeNode root) {
-       HashSet<Integer>list = new HashSet<>();
-       int max=-1309;
         HashMap<Integer,Integer> map = new HashMap<>();
         order(root,map);
-         for(int i:map.values()){
+        int max=-1000;
+        ArrayList<Integer> l = new ArrayList<>();
+        for(int i: map.values()){
             max=Math.max(max,i);
         }
-        for(Map.Entry<Integer,Integer> entry : map.entrySet()){
-            if(entry.getValue() == max){
-                list.add(entry.getKey());
+        for(Map.Entry<Integer,Integer> e: map.entrySet()){
+            if(e.getValue()==max){
+                l.add(e.getKey());
             }
         }
-        int []arr= new int[list.size()];
-        ArrayList<Integer> l1 = new ArrayList<>(list);
-        for(int i =0; i<l1.size(); i++){
-            arr[i]= l1.get(i);
+        int [] arr= new int[l.size()];
+        for(int i =0;i<arr.length;i++){
+            arr[i]=l.get(i);
         }
         return arr;
     }
