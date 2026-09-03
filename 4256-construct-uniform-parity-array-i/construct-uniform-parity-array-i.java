33c527997c91e020aac3ed64d72f5cff371d0f6f
HARSHIT <harshit6683@gmail.com>
2026-09-03 21:41:43 +0530

    Time: 7 ms (0.20%) | Memory: 45.5 MB (16.17%) - LeetSync

--- 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.java
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.java
@@ -1,5 +1,67 @@
 class Solution {
     public boolean uniformArray(int[] nums1) {
+
+        int[] nums2 = new int[nums1.length];
+
+        boolean odd = false;
+        boolean even = false;
+
+        // First option for nums2[0]
+        nums2[0] = nums1[0];
+
+        if (nums2[0] % 2 == 0) {
+            even = true;
+        } else {
+            odd = true;
+        }
+
+        // Second option for nums2[0]
+        for (int j = 1; j < nums1.length; j++) {
+
+            nums2[0] = nums1[0] - nums1[j];
+
+            if (nums2[0] % 2 == 0) {
+                even = true;
+            } else {
+                odd = true;
+            }
+        }
+
+        // Check remaining elements
+        for (int i = 1; i < nums1.length; i++) {
+
+            boolean found = false;
+
+            for (int j = 0; j < nums1.length; j++) {
+
+                if (i == j) {
+                    continue;
+                }
+
+                // First option: nums2[i] = nums1[i]
+                if (even && nums1[i] % 2 == 0) {
+                    found = true;
+                }
+
+                if (odd && nums1[i] % 2 != 0) {
+                    found = true;
+                }
+
+                // Second option: nums2[i] = nums1[i] - nums1[j]
+                if (even && (nums1[i] - nums1[j]) % 2 == 0) {
+                    found = true;
+                }
+
+                if (odd && (nums1[i] - nums1[j]) % 2 != 0) {
+                    found = true;
+                }
+            }
+
+            if (!found) {
+                return false;
+            }
+        }
+
         return true;
     }
 }
